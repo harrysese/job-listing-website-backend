7fbdcd869c56a8b91a53b8011df8b3fda9dce26d
harrysese <tokonisese@gmail.com>
2025-01-06 15:11:01 +0100

    The create method now checks if a username and email exists before creating a user object

--- joblisting_backend/serializers.py
+++ joblisting_backend/serializers.py
@@ -13,14 +13,19 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields=['username', 'email', 'first_name', 'last_name', 'password']
         extra_kwargs={'password':{'write_only':True}}
     def create(self, validated_data):
-            user=User.objects.create_user(
+        if User.objects.filter(username=validated_data['username']).exists():
+            raise serializers.ValidationError({'error':'Username already exists'})
+        elif User.objects.filter(username=validated_data['email']).exists():
+            raise serializers.ValidationError({'error':'Email already exists'})
+        
+        user=User.objects.create_user(
                 username=validated_data['username'],
                 email=validated_data['email'],
                 password=validated_data['password'],
                 first_name=validated_data['first_name'],
                 last_name=validated_data['last_name'],
             )
-            return user
+        return user
 class LoginSerializer(serializers.Serializer):
     username=serializers.CharField()
     password=serializers.CharField(write_only=True)
